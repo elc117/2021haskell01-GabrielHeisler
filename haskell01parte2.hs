b1c2f6c3eda7d2874062c5f93a1eb4e5358b8570
Gabriel Vinícius Heisler <61278049+GabrielHeisler@users.noreply.github.com>
2021-05-26 09:07:35 -0300

    Update haskell01parte2.hs

--- haskell01parte2.hs
+++ haskell01parte2.hs
@@ -7,4 +7,38 @@ htmlItem str = "<li>" ++ str ++ "</li>"
 itemize :: [String] -> [String]
 itemize str = map htmlItem str
 
---onlyVowels :: String -> String
+isA :: Char -> Bool
+isA char = char == 'a'
+onlyVowels :: String -> String
+onlyVowels str = filter isA str
+
+is65p :: Int -> Bool
+is65p age = age > 65
+onlyElderly :: [Int] -> [Int]
+onlyElderly ages = filter is65p ages
+
+isLongWord :: String -> Bool
+isLongWord s = length s > 10
+onlyLongWords :: [String] -> [String]
+onlyLongWords str = filter isLongWord str
+
+isEven :: Int -> Bool
+isEven a = mod a 2 == 0
+onlyEven :: [Int] -> [Int]
+onlyEven num = filter isEven num
+
+between60and80 :: Int -> Bool
+between60and80 x = x>=60 && x<=80
+onlyBetween60and80 :: [Int] -> [Int]
+onlyBetween60and80 lst = filter between60and80 lst
+
+isSpace :: Char -> Bool
+isSpace c = c == ' '
+countSpaces :: String - > Int
+countSpaces str2 = length(filter isSpace str2)
+
+calcAreas :: [Float] -> [Float]
+calcAreas raios = raios^2 * 3.1416
+
+charFound :: Char -> String -> Bool
+charFound c s = map s c

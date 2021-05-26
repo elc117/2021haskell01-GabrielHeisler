ba7df51a99b23e86e3fd0833998a71b629013afe
GabrielHeisler <>
2021-05-26 04:53:32 +0000

    update parte 2

--- haskell01parte2.hs
+++ haskell01parte2.hs
@@ -1,5 +1,8 @@
 -- Prática 01 de Haskell - Parte 2
 -- Nome: Gabriel Vinícius Heisler
 
+htmlItem :: String -> String
+htmlItem str = "<li>" ++ str ++ "</li>"
+
 itemize :: [String] -> [String]
 itemize str = map htmlItem str
